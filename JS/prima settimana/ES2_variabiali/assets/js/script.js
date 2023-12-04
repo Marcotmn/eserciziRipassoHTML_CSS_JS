c4893845dd36b2176bdee4a14e6017646c6bc52c
Marcotmn <129113184+Marcotmn@users.noreply.github.com>
2023-12-04 12:53:39 +0100

    Add files via upload

---
+++ JS/prima settimana/ES2_variabiali/assets/js/script.js
@@ -0,0 +1,36 @@
+////2
+const name = "Marco";
+document.getElementById("name").innerHTML = "CIAO SONO MARCO";
+console.log(name);
+
+////3
+var n1 = 12;
+var n2 = 20;
+console.log(n1 + n2);
+//
+function somma(n1, n2) {
+  return n1 + n2;
+}
+console.log(somma(n1, n2));
+
+////4
+var x = 12;
+
+////5
+//const name = "Tumminia";
+
+////6
+function sottrazione(x) {
+  return x - 4;
+}
+console.log(sottrazione(x, 4));
+//
+console.log(x - 4);
+
+////7
+name1 = "john";
+name2 = "John";
+
+console.log(name1 == name2);
+
+console.log(name1.toLowerCase() == name2.toLowerCase());
